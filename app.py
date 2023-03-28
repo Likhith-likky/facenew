22b4881b51463b1e89f639f67ecbbbb4a2ac9459
Likhith <80377697+Likhith-likky@users.noreply.github.com>
2023-03-28 13:21:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import subprocess
 from flask import Flask
-from main import *
+#from main import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return subprocess.run(["python", "main.py"])
     # fun()
+    #return "Hello"
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
+
